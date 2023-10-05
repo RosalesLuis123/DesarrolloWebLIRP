3c325f313dda2bf935d767126f2790356fd3dea4
RosalesLuis123 <103294514+RosalesLuis123@users.noreply.github.com>
2023-10-04 21:48:50 -0400

    .

--- Laboratorios/Lab3/Ejercicio2/conexion.php
+++ Laboratorios/Lab3/Ejercicio2/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$con = new mysqli("localhost:8081", "root", "", "bd_hotel");
+$con = new mysqli("localhost", "root", "", "bd_hotel");
 if ($con->connect_error) {
     die("conexion fallada" . $con->connect_error);
 }
